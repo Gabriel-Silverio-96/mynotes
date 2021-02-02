001ea625dbba3747b5e769cd98577ca6a1f3be5b
Silvério <contato@gabrielsilver.com.br>
2021-02-02 20:25:08 -0300

    adicionado pagina inicial

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -1,7 +1,27 @@
+import React from 'react';
+
+//components
+import Header from '../components/Header';
+import Title from '../components/UI/Title/index';
+import ButtonPrimary from '../components/UI/Button';
+import Notes from '../components/UI/Notes';
+
+//style
+import '../assets/css/global.css';
+
 function Index() {
   return (
-    <div>
-      <h1>Página inicial</h1>
+    <div className="container"> 
+      <Header />
+
+      <div className="title-area">
+        <Title text="Notas rápidas" />
+        <ButtonPrimary name="Nova nota" className="btn-margin" onClick={() =>  console.log(">>>>>TOP")}  />
+      </div>
+
+      
+        <Notes />
+      
     </div>
   );
 }
