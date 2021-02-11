76c6d0dd8aeeea754844ee9b9cc166862d2b47b5
Silvério <contato@gabrielsilver.com.br>
2021-02-10 21:12:46 -0300

    criado componente de botao deletar

--- src/components/UI/Button/index.jsx
+++ src/components/UI/Button/index.jsx
@@ -3,15 +3,28 @@ import PropTypes from 'prop-types';
 
 import "./style.css";
 
-export default function ButtonPrimary({name, onClick, className}) {   
+function ButtonPrimary({name, onClick, className, type}) {   
     return (
         <button 
+        type={type}
         className={`btn btn-primary ${className}`} 
         onClick={onClick}>
             {name}
         </button>
     )
 }
+
+function Delete({name, onClick, className}) {   
+    return (
+        <button 
+        className={`btn btn-delete ${className}`} 
+        onClick={onClick}>
+            {name}
+        </button>
+    )
+}
+
+export {ButtonPrimary, Delete};
 
 ButtonPrimary.propTypes = {
     name: PropTypes.string.isRequired
