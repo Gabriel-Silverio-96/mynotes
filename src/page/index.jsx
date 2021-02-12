24b5fec19d3a4cdd808b84bd2e629b8cb1d67d3b
Silvério <contato@gabrielsilver.com.br>
2021-02-11 21:07:53 -0300

    configuracao das notas

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -2,106 +2,157 @@ import React, { useEffect, useState, useContext } from 'react';
 import { ContextGlobal } from '../provider/context';
 
 //components
-import Header from '../components/Header';
+import Header from '../components/Header/index';
 import Title from '../components/UI/Title/index';
-import { ButtonPrimary, Delete} from '../components/UI/Button';
+import { ButtonPrimary, Delete } from '../components/UI/Button';
 import Notes from '../components/UI/Notes';
 import Modal from '../components/Modal';
 
-//style
+//assets
 import '../assets/css/global.css';
+import ImageNote from '../assets/image/notes.png';
 
 function Index() {
-  const {modalState, SetModalState} = useContext(ContextGlobal);
-  
+  //Context 
+  const { 
+    modalState,
+    SetModalState,
+    blur,
+    SetBlur 
+  } = useContext(ContextGlobal);
+
   const date = new Date()
   const day = date.getDate();
   const month = date.getMonth() + 1;
   const year = date.getFullYear();
-  const dateNote = `${day < 10 ? "0" + day : day}/${month < 10 ? "0" + month : month}/${year}`
+  const dateNote = `${day < 10 ? "0" + day : day}/${month < 10 ? "0" + month : month}/${year}`;
 
   const [newNote, SetNewNote] = useState(
     {
       id: '',
-      colornote: '',
+      colornote: '#FF51B5',
       title: '',
       date: dateNote,
     },
   );
 
-  function handleChange(e) {
-    SetNewNote({ ...newNote, 
-      id: Math.floor(Math.random() * 1000),
-      [e.target.name]: e.target.value 
-    })
-  }
-
-  function SaveNote(e) {
-    e.preventDefault()
-    
-    SetNewNote({
-      ...newNote
-    })
-
-    const noteStorage = JSON.parse(localStorage.getItem('notes'));
-    if (noteStorage !== null) {
-      localStorage.setItem('notes', JSON.stringify([...noteStorage, newNote]))
-    } else {
-      localStorage.setItem('notes', JSON.stringify([newNote]))
-    }
-
-    CloseModal()       
-    e.target.reset()
-  }
-
   const storage = JSON.parse(localStorage.getItem('notes'));
-  const [noteStorage, SetNoteStorage] = useState(storage)
+  const [noteStorage, SetNoteStorage] = useState(storage);
+  const [erroMessage, SetErroMessage] = useState({
+    inputRequiredTitle: ""
+  });
 
-  useEffect(() => {    
+  //Set State newNote
+  function handleChange(e) {
+    SetNewNote({
+      ...newNote,
+      id: Math.floor(Math.random() * 1000),
+      [e.target.name]: e.target.value
+    })
+  }
+
+  //Submit
+  function SaveNote(e) {
+    e.preventDefault();
+
+    if (newNote.title === "" || newNote.length < 3) {
+      SetErroMessage({
+        ...erroMessage,
+        inputRequiredTitle: "Adicione um título com no mínimo 3 caracteres"
+      })
+    } else {
+      SetNewNote({
+        ...newNote
+      })
+
+      //reset mensagem erro
+      SetErroMessage({})
+
+      const storage = JSON.parse(localStorage.getItem('notes'));
+      if (storage !== null) {
+        localStorage.setItem('notes', JSON.stringify([...storage, newNote]))
+      } else {
+        //Set o primeiro storage do usuário
+        localStorage.setItem('notes', JSON.stringify([newNote]))
+      }
+
+      CloseModal();
+
+      SetNewNote({
+        id: '',
+        colornote: '#FF51B5',
+        title: '',
+        date: dateNote,
+      })
+
+      //esvazia os campos do formulário
+      e.target.reset();
+    }
+  }
+
+  useEffect(() => {
     return SetNoteStorage(storage)
   }, [newNote]);
-  
-  function deleteNotes() {
+
+  function deleteAllNotes() {
     localStorage.removeItem("notes");
     SetNoteStorage(null);
   }
 
   function ShowModal() {
-    SetModalState("modal-show")
+    SetModalState("modal-show");
+    SetBlur("blur");
   }
 
   function CloseModal() {
-    SetModalState("modal-none")
+    SetModalState("modal-none");
+    SetBlur("");
   }
 
   function DeleteThisNote(e) {
-      
-      storage.filter((value, index) => {         
-        if(value.id === Number(e.target.id)) {
-          console.log(e.target);
-        }
-      })
+    noteStorage.filter((value, index) => {
+      if (value.id === Number(e.target.id)) {
+
+        storage.splice(index, index);
+        localStorage.setItem('notes', JSON.stringify([...storage]));
+
+        SetNoteStorage(storage);
+      }
+
+      if (value.id === Number(e.target.id) && index === 0) {
+        //remove a primeira nota
+        storage.shift();
+        localStorage.setItem('notes', JSON.stringify([...storage]));
+
+        SetNoteStorage(storage);
+      }
+
+      if (storage.length === 0 && index === 0) {
+        //remote o item notes do storage
+        deleteAllNotes();
+      }
+    })
   }
 
-  return (      
+  return (    
     <div className="container">
-      <Header />
+      <Header className={blur} />
 
-      <div className="title-area">
+      <div className={`title-area ${blur}`}>
         <Title text="Notas rápidas" />
         <div>
           {noteStorage !== null ? (
-            <Delete name="Deletar todas as notas" className="btn-margin" onClick={deleteNotes} />
+            <Delete name="Deletar todas as notas" className="btn-margin" onClick={deleteAllNotes} />
           ) : ("")}
           <ButtonPrimary name="Nova nota" className="btn-margin" onClick={ShowModal} />
         </div>
       </div>
 
-      <div className="notes-area">
+      <div className={`notes-area ${blur}`}>
         {noteStorage !== null ?
           noteStorage.map((value, index) => (
             <Notes
-              onClick={DeleteThisNote}
+              onDoubleClick={DeleteThisNote}
               key={index}
               id={value.id}
               colornote={value.colornote}
@@ -109,9 +160,12 @@ function Index() {
               date={value.date}
             />
           )) : (
-            <p>Você não tem nenhuma nota</p>
+            <div className="no-note">
+              <img src={ImageNote} alt="Image notas" />
+              <h1>Nenhuma nota</h1>
+              <p>Crie uma nova nota</p>
+            </div>
           )}
-
       </div>
 
       <Modal
@@ -124,7 +178,7 @@ function Index() {
             <label>Selecione uma cor</label>
             <div className="radio-area">
               <div className="color-radio">
-                <input type="radio" id="color1" name="colornote" value="#FF51B5" onChange={handleChange} />
+                <input type="radio" id="color1" name="colornote" value="#FF51B5" onChange={handleChange} defaultChecked />
                 <label htmlFor="color1"></label>
               </div>
               <div className="color-radio">
@@ -149,13 +203,13 @@ function Index() {
               </div>
             </div>
           </div>,
-          <input type="text" name="title" onChange={handleChange} placeholder="Título da nota" />,           
+          <input type="text" name="title" onChange={handleChange} placeholder="Título da nota" maxLength="65" />,
+          <span className="erro-message">{erroMessage.inputRequiredTitle}</span>,
         ]}
         modalFooter={[
           <ButtonPrimary name="Salvar" className="btn-width" />,
         ]}
       />
-
     </div>
   );
 }
