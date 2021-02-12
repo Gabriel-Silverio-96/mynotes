7bd316bd2740b9f49a42157126fbfe4ba249a126
Silvério <contato@gabrielsilver.com.br>
2021-02-11 21:09:39 -0300

    remocao linha em branco

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import Routes from './routes';
 import { ContextProvider } from './provider/context';
 
-
 ReactDOM.render(
   <React.StrictMode>
     <ContextProvider>
