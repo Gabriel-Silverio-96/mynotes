1aaf34003d98cd5b8bd81e348784274c62ddfe10
Silvério <contato@gabrielsilver.com.br>
2021-09-11 12:52:16 -0300

    chore: removed import

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import * as variables from "assets/styles/variables";
 
 export const HeaderContainer = styled.header`
     background-color: ${props => props.theme.colors.backgroundPrimary};
