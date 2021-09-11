eaf67b1f3ee116ac8004013f281913dabad30bc3
Silvério <contato@gabrielsilver.com.br>
2021-09-11 15:17:18 -0300

    fix: removed useState

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { ContextGlobal } from "provider/context";
 
 import { ButtonPrimary, ButtonRound, ButtonSecodary } from "components/UI/Button";
