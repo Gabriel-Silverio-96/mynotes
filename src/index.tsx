db9c032d59e66000dd62655546eeb928d5941b3e
Silvério <contato@gabrielsilver.com.br>
2021-09-15 19:39:44 -0300

    chore: convert jsx to tsx

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./routes";
+
+import { ContextProvider } from "./provider/context";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ContextProvider>      
+      <Routes />
+    </ContextProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
