e47e0a24167b287f89deb5205054f9a33f9bcf49
Silvério <contato@gabrielsilver.com.br>
2021-10-07 22:21:01 -0300

    fix: theme title

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,15 +1,14 @@
 import React from "react";
+
 import GlobalStyles from "assets/styles/global";
 import { ThemeProvider } from "styled-components";
-import useThemeStorage from "util/useThemeStorage";
 
 import dark from "assets/styles/themes/dark";
+import { LayoutProps } from "./types";
 
-const Layout: React.FC = ({ children }) => {
-    const [theme] = useThemeStorage("theme", dark);
-
+const Layout: React.FC<LayoutProps> = ({ children, themeStyle }) => {
     return (
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={themeStyle === undefined ? dark : themeStyle }>
             <GlobalStyles />
             {children}
         </ThemeProvider>
