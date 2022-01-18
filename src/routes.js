4cc9c1033d386e61876f2566fd014d5e0c24078f
Silvério <contato@gabrielsilver.com.br>
2022-01-17 22:04:49 -0300

    refactor: import component

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Switch } from "react-router-dom";
 import CustomRoute from "util/customRoute";
 
 //Pages
-import HomePagePublic from "page/public/Home";
+import Home from "page/public/Home";
 import Login from "page/public/Login";
 import MyNotes from "page/private/myNotes";
 import Profile from "page/private/profile";
@@ -15,7 +15,7 @@ const Router = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <CustomRoute exact path="/" component={HomePagePublic} />             
+                <CustomRoute exact path="/" component={Home} />             
                 <CustomRoute path="/auth/login" component={Login} />             
                 <CustomRoute path="/auth/create-account" component={CreateAccount} />
                 <CustomRoute path="/auth/forgot-password" component={ForgotPassword} />
