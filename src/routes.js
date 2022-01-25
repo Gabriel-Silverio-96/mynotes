3ae50c54bcd358f9e50ca32be2c4cd37a945eb06
Silvério <contato@gabrielsilver.com.br>
2022-01-24 21:29:14 -0300

    test: deploy

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,11 @@ import { BrowserRouter, Switch } from "react-router-dom";
 import Page404 from "page/error/404";
 import MyNotes from "page/private/myNotes";
 import Profile from "page/private/profile";
-import CreateAccount from "page/public/CreateAccount/CreateAccount";
-import ForgotPassword from "page/public/ForgotPassword/ForgotPassword";
-import Home from "page/public/Home/Home";
-import Login from "page/public/Login/Login";
-import ResetPassword from "page/public/ResetPassword/ResetPassword";
+import CreateAccount from "page/public/CreateAccount";
+import ForgotPassword from "page/public/ForgotPassword";
+import Home from "page/public/Home";
+import Login from "page/public/Login";
+import ResetPassword from "page/public/ResetPassword";
 import CustomRoute from "util/customRoute";
 
 const Router = () => {
