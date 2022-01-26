8aeb973452c000d7dcc1c079a3dd7b3147f1072e
Silvério <contato@gabrielsilver.com.br>
2022-01-26 18:12:57 -0300

    refactor(home): name paste

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import MyNotes from "page/private/myNotes";
 import Profile from "page/private/profile";
 import CreateAccount from "page/public/createAccount";
 import ForgotPassword from "page/public/ForgotPassword";
-import Home from "page/public/home";
+import Home from "page/public/Home";
 import Login from "page/public/login";
 import ResetPassword from "page/public/resetPassword";
 import CustomRoute from "util/customRoute";
