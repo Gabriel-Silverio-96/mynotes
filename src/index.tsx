9000708b8e79b008d226e2c142381a93863dd48d
Silvério <contato@gabrielsilver.com.br>
2022-02-03 20:30:12 -0300

    feat(redux): provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,23 @@
+import { ThemeNameProvider } from "provider/theme";
 import React from "react";
 import ReactDOM from "react-dom";
-import Routes from "./routes";
-
+import { Provider } from "react-redux";
 import { AuthProvider } from "./provider/authContext";
 import { ContextProvider } from "./provider/context";
-import { ThemeNameProvider } from "provider/theme";
+import Routes from "./routes";
+import { store } from "common/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeNameProvider>
-      <AuthProvider>
-        <ContextProvider>
-          <Routes />
-        </ContextProvider>
-      </AuthProvider>
-    </ThemeNameProvider>
+    <Provider store={store}>
+      <ThemeNameProvider>
+        <AuthProvider>
+          <ContextProvider>
+            <Routes />
+          </ContextProvider>
+        </AuthProvider>
+      </ThemeNameProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
