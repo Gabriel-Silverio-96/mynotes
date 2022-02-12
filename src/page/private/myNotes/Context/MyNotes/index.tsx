454ec90b95eb5e6de58516fd18e68fe3ca654e6b
Silvério <contato@gabrielsilver.com.br>
2022-02-12 17:57:30 -0300

    feat(mynotes): implement provider

---
+++ src/page/private/myNotes/Context/MyNotes/index.tsx
@@ -0,0 +1,45 @@
+import { IErrorInputMessage } from "common/types/ErrorResponse";
+import { INote } from "common/types/_MyNotes/notes";
+import React, { createContext, useState } from "react";
+
+export const ContextMyNotes = createContext({} as any);
+
+export const DIALOG_DELETE_NOTE_INITIAL_STATE = { delete_just_one_note: true, open: false };
+export const CREATE_NOTE_INITIAL_STATE = { color_note: "#9C10FF", title_note: "", observation: "" };
+
+export const ContextMyNotesProvider = (props: any) => {
+    const [createNote, setCreateNote] = useState<INote>(CREATE_NOTE_INITIAL_STATE);
+    const [errorInputMessage, setErrorInputMessage] = useState<IErrorInputMessage[]>([]);
+    const [isLoadingRequest, setIsLoadingRequest] = useState<boolean>(false);
+    const [noNotesCreated, setNoNotesCreated] = useState<boolean>(true);
+    const [isOpenDialogDeleteNote, setOpenDialogDeleteNote] = useState<any>(DIALOG_DELETE_NOTE_INITIAL_STATE);
+    const [isOpenDialogCreateNote, setOpenDialogCreateNote] = useState<boolean>(false);
+    const [noteIdSelected, setNoteIdSelected] = useState<string>("");
+    const [refreshRequest, setRefreshRequest] = useState<boolean>(false);
+
+    return (
+        <ContextMyNotes.Provider value={
+            {
+                createNote,
+                setCreateNote,
+                errorInputMessage,
+                setErrorInputMessage,
+                isLoadingRequest,
+                setIsLoadingRequest,
+                noNotesCreated,
+                setNoNotesCreated,
+                isOpenDialogDeleteNote, 
+                setOpenDialogDeleteNote,
+                isOpenDialogCreateNote,
+                setOpenDialogCreateNote,
+                noteIdSelected, 
+                setNoteIdSelected,
+                refreshRequest, 
+                setRefreshRequest
+            }
+        }
+        >
+            {props.children}
+        </ContextMyNotes.Provider>
+    )
+}
