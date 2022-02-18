b1eb39aebccaa8a9396c2ba1539cf3548aeb2ba9
Silvério <contato@gabrielsilver.com.br>
2022-02-18 20:29:26 -0300

    feat(dropdown): changed padding

--- src/components/DropdownHeader/styled.ts
+++ src/components/DropdownHeader/styled.ts
@@ -42,7 +42,7 @@ export const DropdownHeaderWrapper = styled.div`
 export const ButtonDropdown = styled.div<ButtonDropdownProps>`
     border: 0.03rem solid ${variables.grayBorder};
     font-size: 0.9rem;
-    padding: 0.57rem 0.7rem;
+    padding: 0.5rem 0.6rem;
     display: flex;
     align-items: center;
     gap: 0.5rem;
