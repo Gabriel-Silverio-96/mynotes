41338d9716d5d0ef100bf1c71411f9583d25dc3c
Silvério <contato@gabrielsilver.com.br>
2022-02-20 11:42:25 -0300

    refactor(form-group): remove

--- src/components/Form/FormGroup/FormGroup.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import FormGroupView from "./FormGroupView";
-
-const FormGroup: React.FC = ({children}) => {
-    return <FormGroupView {... {children}} />
-}
-
-export default FormGroup;
