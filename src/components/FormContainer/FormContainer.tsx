04505041e2b44348b9ad963cc972b67c78084d59
Silvério <contato@gabrielsilver.com.br>
2022-02-21 21:14:32 -0300

    refactor(form-generic): implement form container

---
+++ src/components/FormContainer/FormContainer.tsx
@@ -0,0 +1,16 @@
+import { ContextTheme } from "provider/theme";
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import FormContainerView from "./FormContainerView";
+import { IFormContainer } from "./types/types.component";
+
+const FormContainer: React.FC<Omit<IFormContainer, "history" | "themeTitle">> = (props) => {
+    const history = useHistory();
+    const { themeContext } = useContext(ContextTheme);
+    const themeTitle = themeContext.title;
+    const { title, children, widthModal = "25rem", isLogoVisible = false, isActiveButtonBack = true } = props;
+
+    return <FormContainerView {... { history, title, themeTitle, children, widthModal, isLogoVisible, isActiveButtonBack }} />
+}
+
+export default FormContainer;
