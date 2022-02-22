9407dcc96ce93ccc1af18afffcd2c8a89fffebb6
Silvério <contato@gabrielsilver.com.br>
2022-02-21 22:01:38 -0300

    refactor(profile): implement form container

--- src/page/private/profile/ProfileView.tsx
+++ src/page/private/profile/ProfileView.tsx
@@ -1,20 +1,21 @@
 import { IErrorInputMessage } from "common/types/ErrorResponse";
 import Button from "components/Button";
 import Input from "components/FormFields/Input";
-import FormGeneric from "components/FormGeneric";
+import FormContainer from "components/FormContainer";
 import React from "react";
 import { IProfile } from "./types";
 
 const ProfileView: React.FC<IProfile> = (props) => {
-    const { isLoadingData, isLoadingRequestEditing, errorInputMessage, putEditProfile, userData, handleChange } = props;
+    const {
+        isLoadingData,
+        isLoadingRequestEditing,
+        errorInputMessage,
+        putEditProfile,
+        userData,
+        handleChange
+    } = props;
     return (
-
-        <FormGeneric
-            title="Profile"
-            widthModal="25rem"
-            isHeaderActive={false}
-            isActiveBack={true}
-        >
+        <FormContainer title="Profile" disabledButtonBack={isLoadingData || isLoadingRequestEditing}>
             <form method="post" onSubmit={putEditProfile}>
                 <Input
                     label="Full name"
@@ -39,9 +40,15 @@ const ProfileView: React.FC<IProfile> = (props) => {
                     disabled={true}
                 />
 
-                <Button type="submit" title="Save" isLoading={isLoadingRequestEditing} messageLoading="Saving" />
+                <Button 
+                    type="submit" 
+                    title="Save" 
+                    disabled={isLoadingData} 
+                    isLoading={isLoadingRequestEditing} 
+                    messageLoading="Saving" 
+                />
             </form>
-        </FormGeneric>
+        </FormContainer>
     )
 }
 
