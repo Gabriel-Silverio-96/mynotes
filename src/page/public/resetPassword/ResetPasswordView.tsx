c031c733db8bfb95c127537f0e2a93178f99ec01
Silvério <contato@gabrielsilver.com.br>
2022-02-21 21:49:52 -0300

    refactor(reset-password): implement form container

--- src/page/public/resetPassword/ResetPasswordView.tsx
+++ src/page/public/resetPassword/ResetPasswordView.tsx
@@ -1,7 +1,6 @@
 import Button from "components/Button";
 import Input from "components/FormFields/Input";
-import FormGeneric from "components/FormGeneric";
-import Layout from "components/Layout";
+import FormContainer from "components/FormContainer";
 import React from "react";
 import { Link } from "react-router-dom";
 import { SendingMessage } from "./styled";
@@ -16,36 +15,33 @@ const ResetPasswordView: React.FC<IResetPassword> = (props) => {
         resetPassword
     } = props;
     return (
-        <Layout themeSwitch={false}>
-            <FormGeneric title="Reset password" widthModal="25rem" isHeaderActive={true} isActiveBack={false}>
-
-                {!resetPasswordSuccessfully ? (
-                    <>
-                        <p>Enter your new password with at least 8 characters</p>
-                        <form method="post" onSubmit={resetPassword}>
-                            <Input
-                                id="password"
-                                label="New password"
-                                name="password"
-                                typeInput="password"
-                                onChange={handleChange}
-                                errorMessage={errorInputMessage.length > 0 ? errorInputMessage[0].msg : ""}
-                            />
-                            <Button title="Save" type="submit" isLoading={isLoading} />
-                        </form>
-                    </>
-                ) : (
-                    <SendingMessage>
-                        <p>
-                            Your password has been reset successfully 😊
-                        </p>
-                        <Link to="/auth/login">
-                            <Button title="Back to login" />
-                        </Link>
-                    </SendingMessage>
-                )}
-            </FormGeneric>
-        </Layout>
+        <FormContainer title="Reset password" isLogoVisible isActiveButtonBack={false}>
+            {!resetPasswordSuccessfully ? (
+                <>
+                    <p>Enter your new password with at least 8 characters</p>
+                    <form method="post" onSubmit={resetPassword}>
+                        <Input
+                            id="password"
+                            label="New password"
+                            name="password"
+                            typeInput="password"
+                            onChange={handleChange}
+                            errorMessage={errorInputMessage.length > 0 ? errorInputMessage[0].msg : ""}
+                        />
+                        <Button title="Save" type="submit" isLoading={isLoading} />
+                    </form>
+                </>
+            ) : (
+                <SendingMessage>
+                    <p>
+                        Your password has been reset successfully 😊
+                    </p>
+                    <Link to="/auth/login">
+                        <Button title="Back to login" />
+                    </Link>
+                </SendingMessage>
+            )}
+        </FormContainer>
     )
 }
 
