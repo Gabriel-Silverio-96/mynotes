37db0e6ed4ff10b47bce4d3fd93301fc32d367b8
Silvério <contato@gabrielsilver.com.br>
2022-02-27 19:21:21 -0300

    refactor(forgot-password): snackbar

--- src/page/public/ForgotPassword/ForgotPassword.tsx
+++ src/page/public/ForgotPassword/ForgotPassword.tsx
@@ -1,8 +1,6 @@
 import { AxiosError, AxiosResponse } from "axios";
-import { snackBar } from "common/store/snackBar/snackBar.action";
 import { IDataErrorResponse, IDataMessageResponse, IErrorInputMessage } from "common/types/ErrorResponse";
 import React, { ChangeEvent, FormEvent, useState } from "react";
-import { useDispatch } from "react-redux";
 import apiMyNotes from "service/apiMyNotes";
 import ForgotPasswordView from "./ForgotPasswordView";
 import { IUserData } from "./types";
@@ -10,8 +8,6 @@ import { IUserData } from "./types";
 const USER_DATA_INPUT_INITIAL_STATE = { email: "" };
 
 const ForgotPassword: React.FC = () => {
-    const dispatch = useDispatch();
-
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [isSendingMessage, setIsSendingMessage] = useState<boolean>(false);
     const [errorInputMessage, setErrorInputMessage] = useState<IErrorInputMessage[]>([]);
@@ -29,18 +25,14 @@ const ForgotPassword: React.FC = () => {
         setIsLoading(true);
 
         try {
-            const { data } = await apiMyNotes.post("auth/forgot-password", userData) as AxiosResponse<IDataMessageResponse>;
+            await apiMyNotes.post("auth/forgot-password", userData) as AxiosResponse<IDataMessageResponse>;
             setIsSendingMessage(true);
-            setIsLoading(false);
-            dispatch(snackBar(true, data.message, data.type_message));
         } catch (err) {
             const error = err as AxiosError;
             const { status, data } = error.response as AxiosResponse<IDataErrorResponse>;
-
             if (status === 400) setErrorInputMessage(data.errors);
-            if (status === 403 || status === 500) dispatch(snackBar(true, data.message, data.type_message));
-
-            return setIsLoading(false);
+        } finally {
+            setIsLoading(false);
         }
     };
 
