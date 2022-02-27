d58e2d2caca7996060a4ee5dd7b5754d06a89164
Silvério <contato@gabrielsilver.com.br>
2022-02-27 20:28:51 -0300

    refactor(mynotes-delete-this-note): snackbar

--- src/page/private/myNotes/DialogDeleteThisNote/DialogDeleteThisNote.tsx
+++ src/page/private/myNotes/DialogDeleteThisNote/DialogDeleteThisNote.tsx
@@ -1,16 +1,12 @@
-import { AxiosError, AxiosResponse } from "axios";
-import { snackBar } from "common/store/snackBar/snackBar.action";
-import { IDataErrorResponse } from "common/types/ErrorResponse";
+import { AxiosResponse } from "axios";
 import { ISnackBarResponse } from "common/types/SnackBar";
 import React, { useContext, useState } from "react";
-import { useDispatch } from "react-redux";
 import apiMyNotes from "service/apiMyNotes";
 import useDialogMynotes from "../common/hooks/useDialogMynotes";
 import { ContextMyNotes } from "../Context/MyNotes";
 import DialogDeleteNoteView from "./DialogDeleteThisNoteView";
 
 const DialogDeleteThisNote: React.FC = () => {
-    const dispatch = useDispatch();
     const { closeDialogDeleteThisNote } = useDialogMynotes();
     
     const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -21,13 +17,9 @@ const DialogDeleteThisNote: React.FC = () => {
     const deleteThisNote = async () => {
         setIsLoading(true);
         try {
-            const { data } = await apiMyNotes.delete(`notes/delete-this/note_id=${noteIdSelected}`) as AxiosResponse<ISnackBarResponse>;
-            dispatch(snackBar(true, data.message, data.type_message));                        
+            await apiMyNotes.delete(`notes/delete-this/note_id=${noteIdSelected}`) as AxiosResponse<ISnackBarResponse>;
             setRefreshRequest((prevState: boolean) => !prevState);
         } catch (err) {
-            const error = err as AxiosError;
-            const { data } = error.response as AxiosResponse<IDataErrorResponse>;            
-            dispatch(snackBar(true, data.message, data.type_message));
         } finally {
             setIsLoading(false);
             setOpenDialogDeleteThisNote((prevState: boolean) => !prevState);
