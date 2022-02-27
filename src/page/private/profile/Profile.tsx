d8268322422fdf1c2c8b2791f9ffae76f88f0785
Silvério <contato@gabrielsilver.com.br>
2022-02-27 20:32:33 -0300

    refactor(profile): snackbar

--- src/page/private/profile/Profile.tsx
+++ src/page/private/profile/Profile.tsx
@@ -1,9 +1,7 @@
 import { AxiosError, AxiosResponse } from "axios";
-import { snackBar } from "common/store/snackBar/snackBar.action";
 import { IDataErrorResponse, IErrorInputMessage } from "common/types/ErrorResponse";
 import { ISnackBarResponse } from "common/types/SnackBar";
 import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
 import { useHistory } from "react-router";
 import apiMyNotes from "service/apiMyNotes";
 import ProfileView from "./ProfileView";
@@ -11,7 +9,6 @@ import { IProfile, IUserData } from "./types";
 
 const Profile: React.FC<IProfile> = () => {
     const history = useHistory();
-    const dispatch = useDispatch();
 
     const [userData, setUserData] = useState({} as IUserData);
     const [isLoadingData, setIsLoadingData] = useState<boolean>(true);
@@ -25,17 +22,13 @@ const Profile: React.FC<IProfile> = () => {
                 const { data } = await apiMyNotes.get("/auth/account") as AxiosResponse<IUserData>;
                 setUserData(data);
             } catch (err) {
-                const error = err as AxiosError;
-                const { data, status } = error.response as AxiosResponse<IDataErrorResponse>;
-                if (status >= 404) dispatch(snackBar(true, data.message, data.type_message));
-
             } finally {
                 setIsLoadingData(false);
             }
         }
         getUserData();
         
-    }, [dispatch]);
+    }, []);
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         const name = e.target.name;
@@ -48,8 +41,7 @@ const Profile: React.FC<IProfile> = () => {
         setErrorInputMessage([]);
         setIsLoadingRequestEdit(true);
         try {
-            const { data } = await apiMyNotes.put("auth/account", userData) as AxiosResponse<ISnackBarResponse>;
-            dispatch(snackBar(true, data.message, data.type_message));
+            await apiMyNotes.put("auth/account", userData) as AxiosResponse<ISnackBarResponse>;
             history.push("/mynotes");
         } catch (err) {
             const error = err as AxiosError;
