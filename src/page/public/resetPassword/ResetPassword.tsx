ffc42a4b6e90568993e7abe4f7f2463b5b0e3a22
Silvério <contato@gabrielsilver.com.br>
2022-02-27 20:00:02 -0300

    refactor(reset-password): snackbar

--- src/page/public/resetPassword/ResetPassword.tsx
+++ src/page/public/resetPassword/ResetPassword.tsx
@@ -1,8 +1,6 @@
 import { AxiosError, AxiosResponse } from "axios";
-import { snackBar } from "common/store/snackBar/snackBar.action";
 import { IDataErrorResponse, IDataMessageResponse, IErrorInputMessage } from "common/types/ErrorResponse";
 import React, { ChangeEvent, FormEvent, useState } from "react";
-import { useDispatch } from "react-redux";
 import { useParams } from "react-router";
 import apiMyNotes from "service/apiMyNotes";
 import ResetPasswordView from "./ResetPasswordView";
@@ -11,7 +9,6 @@ import { INewPasswordInputs, Params } from "./types";
 const RESET_PASSWORD_INPUTS_INITIAL_STATE: INewPasswordInputs = { password: "" };
 
 const ResetPassword: React.FC = () => {
-    const dispatch = useDispatch();
     const { token } = useParams<Params>();
 
     const [resetPasswordSuccessfully, setResetPasswordSuccessfully] = useState<boolean>(false);
@@ -30,18 +27,14 @@ const ResetPassword: React.FC = () => {
         setIsLoading(true);
 
         try {
-            const { data } = await apiMyNotes.post(`auth/reset-password/token=${token}`, newPassword) as AxiosResponse<IDataMessageResponse>;
+            await apiMyNotes.post(`auth/reset-password/token=${token}`, newPassword) as AxiosResponse<IDataMessageResponse>;
             setResetPasswordSuccessfully(prevState => !prevState);
-            setIsLoading(false);
-            dispatch(snackBar(true, data.message, data.type_message));
         } catch (err) {
             const error = err as AxiosError;
             const { status, data } = error.response as AxiosResponse<IDataErrorResponse>;
-
-            if (status === 400) setErrorInputMessage(data.errors);
-            if (status === 403 || status === 500) dispatch(snackBar(true, data.message, data.type_message));
-
-            return setIsLoading(false);
+            if (status === 400) setErrorInputMessage(data.errors);            
+        } finally {
+            setIsLoading(false);
         }
     }
 
