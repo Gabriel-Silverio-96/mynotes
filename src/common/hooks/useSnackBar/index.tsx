7c738881215d3efd04996532f8a4281809359415
Silvério <contato@gabrielsilver.com.br>
2022-02-27 10:16:14 -0300

    feat(snack-bar): implement hook

--- src/common/hooks/useSnackBar/index.tsx
+++ src/common/hooks/useSnackBar/index.tsx
@@ -1,20 +1,21 @@
-import { useDispatch } from "react-redux";
 import { snackBar } from "common/store/snackBar/snackBar.action";
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
 
 export default function useSnackBar() {
     const dispatch = useDispatch();
 
-    const snackBarSuccess = (isOpen: boolean, message: string) => {
+    const snackBarSuccess = useCallback((isOpen: boolean, message: string) => {
         dispatch(snackBar(isOpen, message, "success"));
-    }
+    }, [dispatch]);
 
-    const snackBarError = (isOpen: boolean, message: string) => {
+    const snackBarError = useCallback((isOpen: boolean, message: string) => {
         dispatch(snackBar(isOpen, message, "error"));
-    }
+    }, [dispatch]);
 
-    const snackBarWarning = (isOpen: boolean, message: string) => {
+    const snackBarWarning = useCallback((isOpen: boolean, message: string) => {
         dispatch(snackBar(isOpen, message, "warning"));
-    }
+    }, [dispatch]);
 
     return { snackBarSuccess, snackBarError, snackBarWarning }
 }
