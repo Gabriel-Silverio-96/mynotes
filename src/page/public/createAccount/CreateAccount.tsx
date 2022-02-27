59f2cdea964a77e4b1269a5497198d8de7dd44a6
Silvério <contato@gabrielsilver.com.br>
2022-02-27 18:30:22 -0300

    refactor(create-account): snackbar

--- src/page/public/createAccount/CreateAccount.tsx
+++ src/page/public/createAccount/CreateAccount.tsx
@@ -1,8 +1,6 @@
 import { AxiosError, AxiosResponse } from "axios";
-import { snackBar } from "common/store/snackBar/snackBar.action";
 import { IDataErrorResponse, IErrorInputMessage } from "common/types/ErrorResponse";
 import React, { ChangeEvent, FormEvent, useState } from "react";
-import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import apiMyNotes from "service/apiMyNotes";
 import CreateAccountView from "./CreateAccountView";
@@ -11,7 +9,6 @@ import { ICreateAccount, IUserData } from "./types";
 const USER_DATA_INPUTS_INITIAL_STATE: IUserData = { full_name: "", email: "", password: "" };
 
 const CreateAccount: React.FC<ICreateAccount> = () => {
-    const dispatch = useDispatch();
     const history = useHistory();
 
     const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -31,17 +28,14 @@ const CreateAccount: React.FC<ICreateAccount> = () => {
         
         try {
             await apiMyNotes.post("/auth/create-account", userData);
-            dispatch(snackBar(true, "Account created successfully"));
-            setIsLoading(false);
             return history.push("/auth/login");
         } catch (err) {
             const error = err as AxiosError;
             const { status, data } = error.response as AxiosResponse<IDataErrorResponse>;
                 
-            if (status === 400) setErrorInputMessage(data.errors);
-            if (status === 403 || status === 500) dispatch(snackBar(true, data.message, data.type_message));
-            
-            return setIsLoading(false);
+            if (status === 400) setErrorInputMessage(data.errors);            
+        } finally {
+            setIsLoading(false);
         }
     }
 
