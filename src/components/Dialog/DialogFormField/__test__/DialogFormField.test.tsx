26ea347e548c1ec78fb7cd7b564966219c2ebc84
Silvério <contato@gabrielsilver.com.br>
2022-02-28 18:35:50 -0300

    test(dialog-form-filed): implement dialog form filed test

---
+++ src/components/Dialog/DialogFormField/__test__/DialogFormField.test.tsx
@@ -0,0 +1,36 @@
+import { render } from "@testing-library/react";
+import Input from "components/FormFields/Input";
+import React from "react";
+import ReactDOM from "react-dom";
+import ThemeProviderTest from "util/test/ThemeProviderTest";
+import DialogFormField from "../DialogFormField";
+
+const DialogFormFieldRender: React.FC = () => {
+    return (
+        <ThemeProviderTest>
+            <DialogFormField>
+                <Input label="User" name="user" id="user" typeInput="text" data-testid="input-user" />
+            </DialogFormField>
+        </ThemeProviderTest>
+    )
+}
+
+describe("<DialogFormField />", () => {
+    it("Should render the component", () => {
+        const { container } = render(<DialogFormFieldRender />);
+        expect(container).toBeDefined();
+    });
+
+    it("Should unmount the component", () => {
+        const div = document.createElement("div")
+        ReactDOM.render(<DialogFormFieldRender />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it("Should have input children inside <DialogFormField>", () => {
+        const { container, getByTestId } = render(<DialogFormFieldRender />);
+        const inputUser = getByTestId("input-user");
+        
+        expect(container).toContainElement(inputUser);
+    });
+})
