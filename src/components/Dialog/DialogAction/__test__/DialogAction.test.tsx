27fc3d95e0b5248d784f43b7fb10274d0c00f750
Silvério <contato@gabrielsilver.com.br>
2022-02-28 15:58:30 -0300

    test(dialog-action): implement dialog action test

---
+++ src/components/Dialog/DialogAction/__test__/DialogAction.test.tsx
@@ -0,0 +1,35 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import ReactDOM from "react-dom";
+import ThemeProviderTest from "util/test/ThemeProviderTest";
+import DialogAction from "../DialogAction";
+
+const DialogActionRender: React.FC = ({ children }) => {
+    return (
+        <ThemeProviderTest>
+            <DialogAction>
+                {children}
+            </DialogAction>
+        </ThemeProviderTest>
+    )
+}
+
+describe("<DialogAction />", () => {
+    it("Should render the component", () => {
+        const { container } = render(<DialogActionRender />);
+        expect(container).toBeDefined();
+    });
+
+    it("Should unmount the component", () => {
+        const div = document.createElement("div")
+        ReactDOM.render(<DialogActionRender />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it("Should have render <span /> with message 'Test message'", () => {
+        const { getByText } = render(<DialogActionRender><span>Test message</span></DialogActionRender>);
+        const dialogActionChildren = getByText(/test message/i);
+        
+        expect(dialogActionChildren).toBeInTheDocument();
+    });
+})
