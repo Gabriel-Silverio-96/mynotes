e9af8f81325d709743799057758cef04a130d9ff
Silvério <contato@gabrielsilver.com.br>
2022-02-28 18:24:37 -0300

    test(dialog-form): implement dialog form test

---
+++ src/components/Dialog/DialogForm/__test__/DialogForm.test.tsx
@@ -0,0 +1,47 @@
+import { fireEvent, render } from "@testing-library/react";
+import Button from "components/Button";
+import { DialogFormField } from "components/Dialog/DialogFormField/styled";
+import Input from "components/FormFields/Input";
+import React from "react";
+import ReactDOM from "react-dom";
+import ThemeProviderTest from "util/test/ThemeProviderTest";
+import DialogForm from "../DialogForm";
+import { IDialogForm } from "../types";
+
+const DialogFormRender: React.FC<IDialogForm> = ({ onSubmit, method }) => {
+    return (
+        <ThemeProviderTest>
+            <DialogForm onSubmit={onSubmit} method={method}>
+                <DialogFormField>
+                    <Input label="User" name="user" id="user" typeInput="text" data-testid="input-user" />
+                </DialogFormField>
+                <Button title="Submit" />
+            </DialogForm>
+        </ThemeProviderTest>
+    )
+}
+
+describe("<DialogForm />", () => {
+    it("Should render the component", () => {
+        const { container } = render(<DialogFormRender />);
+        expect(container).toBeDefined();
+    });
+
+    it("Should unmount the component", () => {
+        const div = document.createElement("div");
+        ReactDOM.render(<DialogFormRender />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it("Should have input with value 'mynotes' and one called submit button", () => {
+        const onSubmit = jest.fn();
+        const { getByText, getByTestId } = render(<DialogFormRender method="get" onSubmit={onSubmit} />);
+        const buttonSubmit = getByText(/submit/i) as HTMLButtonElement;
+        const inputUser = getByTestId("input-user") as HTMLInputElement;
+        fireEvent.change(inputUser, { target: { value: "mynotes" } });
+        fireEvent.submit(buttonSubmit);
+
+        expect(inputUser).toHaveValue("mynotes");
+        expect(onSubmit).toHaveBeenCalledTimes(1);
+    });
+});
