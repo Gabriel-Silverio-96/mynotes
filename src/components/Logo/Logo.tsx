ed915f919ba834865b9eeb83e91e9f03e4d0231b
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-03-20 18:47:00 -0300

    test(logo): implement logo test

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -3,7 +3,6 @@ import LogoView from "./LogoView";
 import { ILogo } from "./types/types.component";
 
 const Logo: React.FC<ILogo> = ({ themeTitle, responsive }) => {
-
     return <LogoView {... { themeTitle, responsive }} />
 }
 
