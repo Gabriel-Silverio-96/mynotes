ecf8aefca76260b2d3f6ce3f1a05ec9136d61ba9
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-03-26 12:34:07 -0300

    test(page-forgot-password): implement forgot password test

---
+++ src/page/public/ForgotPassword/__test__/mock/index.ts
@@ -0,0 +1,19 @@
+import { IDataMessageResponse } from "common/types/ErrorResponse";
+
+const errorsInputMessage = {
+    errors: [
+        { value: "", msg: "Email is required", param: "email", location: "body" },
+    ]
+};
+
+const unregisteredUser: IDataMessageResponse = {
+    type_message: "warning",
+    message: "Unregistered user, create an account"
+};
+
+const sendEmail: IDataMessageResponse = {
+    type_message: "success",
+    message: "Email send"
+};
+
+export { errorsInputMessage, unregisteredUser, sendEmail };
