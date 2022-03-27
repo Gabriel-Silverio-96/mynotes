5581730e43fba13238fc29adc1db52cf6ed37faf
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-03-27 09:42:16 -0300

    test(custom-render): add custom-render

---
+++ src/util/test/customRender/index.tsx
@@ -0,0 +1,28 @@
+import {
+    render as rtlRender
+} from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import React, { ReactElement } from 'react';
+import { Router } from 'react-router-dom';
+
+interface ICustomRender {
+    route: string;
+    history?: any;
+}
+
+export default function customRender(
+    ui: ReactElement,
+    {
+        route =  "/",
+        history = createMemoryHistory({ initialEntries: [route] }),
+    } = {} as ICustomRender
+) {
+    const Wrapper: React.FC = ({ children }) => (
+        <Router history={history}>{children}</Router>
+    );
+
+    return {
+        ...rtlRender(ui, { wrapper: Wrapper }),
+        history,
+    };
+}
