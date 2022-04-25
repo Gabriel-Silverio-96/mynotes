5980d7f299e235c768ac7e42450e34ca4a82ab55
Gabriel Silvério <53228013+Gabriel-Silverio-96@users.noreply.github.com>
2022-04-25 20:33:31 -0300

    refactor(routes): split responsibilities
    * refactor(routes): remove page component
    
    * refactor(routes): change file extension from .js to .tsx
    
    * refactor(util): create folder custom route
    
    * refactor(routes): implement prop is header visible
    
    * refactor(routes): implement prop is header visible page 404

---
+++ src/routes.tsx
@@ -0,0 +1,31 @@
+import Page404 from "page/error/404";
+import MyNotesPage from "page/private/myNotes";
+import Profile from "page/private/profile";
+import CreateAccount from "page/public/createAccount";
+import ForgotPassword from "page/public/ForgotPassword";
+import Home from "page/public/home";
+import Login from "page/public/login";
+import ResetPassword from "page/public/resetPassword";
+import { BrowserRouter, Switch } from "react-router-dom";
+import CustomRoute from "util/customRoute";
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <CustomRoute exact path="/" component={Home} />
+                <CustomRoute path="/auth/login" component={Login} />
+                <CustomRoute path="/auth/create-account" component={CreateAccount} />
+                <CustomRoute path="/auth/forgot-password" component={ForgotPassword} />
+                <CustomRoute path="/auth/reset-password/token=:token" isHeaderVisible={false} component={ResetPassword} />
+
+                <CustomRoute isPrivate path="/myNotes" component={MyNotesPage} />
+                <CustomRoute isPrivate path="/profile" component={Profile} />
+
+                <CustomRoute path="*" isHeaderVisible={false} component={Page404} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Router;
---
+++ src/util/customRoute/types/types.component.ts
@@ -0,0 +1,7 @@
+import { RouteProps } from "react-router-dom";
+
+export interface ICustomRoute extends RouteProps {
+    isPrivate?: boolean;
+    path?: string;
+    isHeaderVisible?: boolean;
+}
