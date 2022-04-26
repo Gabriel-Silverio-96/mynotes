48291e2d5869ac6de20a8b7db533908c149cb603
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-04-26 20:42:51 -0300

    refactor-test-unit(util): delete util rgbToHex

--- src/util/test/RgbToHex/index.ts
+++
@@ -1,9 +0,0 @@
-const rgbToHex = (rgb: string) => {    
-    const rgbList = rgb.slice(4, -1).split(", ");
-    return '#' + rgbList.map(rgb => {
-        const hex = Number(rgb).toString(16)
-        return hex.length === 1 ? "0" + hex : hex
-    }).join("")
-}
-
-export default rgbToHex;
