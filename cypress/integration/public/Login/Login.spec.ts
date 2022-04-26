24bcff684b5d3b65dc56a5b83754aa18325d0c0d
Gabriel Silvério <53228013+Gabriel-Silverio-96@users.noreply.github.com>
2022-04-25 22:07:59 -0300

    test-cy(login): implement cypress env

--- cypress/integration/public/Login/Login.spec.ts
+++ cypress/integration/public/Login/Login.spec.ts
@@ -5,17 +5,17 @@ describe("Login page", () => {
     });     
 
     it("Incorrect email or password", () => {
-        cy.login("xuxu@mozej.com", "123456789", false);
+        cy.login(Cypress.env("email"), "password123", false);
         cy.contains("Incorrect email or password");
     });
 
     it("User unregistered", () => {
-        cy.login("xuxu1@mozej.com", "123456789", false);
+        cy.login("email-unregistered@email.com", "password123", false);
         cy.contains("Unregistered user, create an account");
     });
 
     it("Success login", () => {
-        cy.login("xuxu@mozej.com", "12345678")
+        cy.login(Cypress.env("email"), Cypress.env("password"));
         cy.contains("Loading");
     });
 })
