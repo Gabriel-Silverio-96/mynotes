6a164a6f86f9bf63d661a5340d72b99b35d862ea
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-06-21 20:57:56 -0300

    refactor(color-picker): types

--- src/components/ColorPicker/types/ColorPicker.component.ts
+++ src/components/ColorPicker/types/ColorPicker.component.ts
@@ -1,9 +1,9 @@
 import React from "react";
 import { Color } from "react-color-palette";
 import { ColorPickerProps } from "react-color-palette/lib/interfaces/ColorPicker.interface";
-import { DefaultTheme, ThemeProps } from "styled-components";
+import { DefaultTheme } from "styled-components";
 
-export interface IColorPicker extends ColorPickerProps {
+export interface IColorPicker extends Omit<ColorPickerProps, "onChange"> {
 	setColor: React.Dispatch<React.SetStateAction<Color>>;
 	open: boolean;
 	loading: boolean;
@@ -11,5 +11,5 @@ export interface IColorPicker extends ColorPickerProps {
 	closeColorPicker: () => void;
 	colorSelect: string;
 	setColorSelect: React.Dispatch<React.SetStateAction<string>>;
-	theme: ThemeProps<DefaultTheme>;
+	theme: DefaultTheme;
 }
