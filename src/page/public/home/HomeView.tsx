10b22dd46c4100a56518808347fe45d530a6ab5d
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-07-01 20:15:52 -0300

    feat(home): implement section cards and footer

--- src/page/public/home/HomeView.tsx
+++ src/page/public/home/HomeView.tsx
@@ -1,21 +1,76 @@
+import Badge from "components/Badge";
 import Button from "components/Button";
+import Logo from "components/Logo";
 import React from "react";
 import { Link } from "react-router-dom";
-import { Main } from "./styled";
+import { Main, MainWrapper, CardContainer,CardHeader, Card, CardContent, Quotes, Footer } from "./styled";
 import { IHome } from "./types";
 
 const HomeView: React.FC<IHome> = () => {
 	return (
 		<Main>
-			<h1>New way<br /> to <span>organize</span></h1>
-			<p>Easily and quickly organize your day</p>
-			<div>
-				<Link to="auth/create-account">
-					<Button size="medium"
-						title="Get started"
-					/>
-				</Link>
-			</div>
+			<MainWrapper>
+				<h1>New way<br /> to <span>organize</span></h1>
+				<p>Easily and quickly organize your day</p>
+				<div>
+					<Link to="auth/create-account">
+						<Button size="medium"
+							title="Get started"
+						/>
+					</Link>
+				</div>
+			</MainWrapper>
+			<CardContainer>
+				<Card>
+					<CardContent>
+						<CardHeader>
+							<h2>Notes</h2>
+						</CardHeader>
+						<p>
+							Create notes to never forget your assignment
+						</p>
+					</CardContent>
+				</Card>
+				<Card>
+					<CardContent>
+						<CardHeader>
+							<h2>Text</h2>
+							<Badge text="Comming soon" variant="default" />
+						</CardHeader>
+						<p>
+							Organize your text quickly using markdown, create lists, tables, to-do lists and more
+						</p>
+					</CardContent>
+				</Card>
+				<Card>
+					<CardContent>
+						<CardHeader>
+							<h2>Kanban</h2>
+							<Badge text="Comming soon" variant="default" />
+						</CardHeader>
+						<p>
+							Control the flow of tasks and activities for
+							your team or project
+						</p>
+					</CardContent>
+				</Card>
+			</CardContainer>
+
+			<Quotes>
+				<h1>For every minute spent organizing, an hour is earned</h1>
+			</Quotes>
+
+			<Footer>
+				<Logo isoType />
+				<div>
+					<Link to="/auth/login">
+						<Button title="Login" variant="text" />
+					</Link>
+					<Link to="/auth/create-account">
+						<Button title="Create account" variant="secondary" />
+					</Link>
+				</div>
+			</Footer>
 		</Main>
 	);
 };
--- src/page/public/home/styled.ts
+++ src/page/public/home/styled.ts
@@ -3,17 +3,21 @@ import * as variables from "assets/styles/variables";
 import { MainProps } from "./types";
 
 export const Main = styled.main<MainProps>`
-    text-align: center;
+    background-image: url(${props => props.src});
+`;
+
+export const MainWrapper = styled.div`
+	text-align: center;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    height: 80vh;
+    height: 100vh;
     position: relative;
-    background-image: url(${props => props.src});
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center;
-    background-color: transparent; // ${props => props.theme.colors.backgroundPrimary}
+    background-color: transparent;
+	margin-bottom: 2rem;
 
     h1 {
         color: ${props => props.theme.colors.textColorTitle};
@@ -73,4 +77,62 @@ export const Main = styled.main<MainProps>`
 			margin-top: 1rem;
 		}
   	}
+`;
+
+export const CardContainer = styled.section`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-wrap: wrap;
+	gap: 2rem;
+`;
+
+export const CardHeader = styled.div`
+	display: flex;
+	align-items: center;
+	gap: 1rem;
+	margin-bottom: 2rem;
+`;
+
+export const Card = styled.div`
+	padding: 2rem;
+	border-radius: 0.5rem;
+	max-width: 22rem;
+	min-height: 10rem;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	background-color: ${props => props.theme.colors.backgroundPrimary};
+	border: 0.01rem solid ${variables.grayMedium2};
+`;
+
+export const CardContent = styled.div`
+	h2 {
+		color: ${props => props.theme.colors.textColorTitle};
+	}
+
+	p {
+		color: ${props => props.theme.colors.textColorParagraph};
+	}
+`;
+
+export const Quotes = styled.div`
+	margin: 10rem 0;
+	padding: 5rem 1rem;
+	border-radius: 0.5rem;
+	text-align: center;
+	background-color: ${variables.primaryColor};
+	color: ${variables.white};
+`;
+
+export const Footer = styled.footer`
+	border: 0.01rem solid ${variables.grayMedium2};
+	border-right: 0;
+	border-left: 0;
+	border-bottom: 0;
+	padding: 5rem 2rem;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
 `;
