716fb49889a957e80de0d90814f212b99f73c2cc
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-07-01 20:14:43 -0300

    feat(logo): implement prop isotype

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import LogoView from "./LogoView";
 import { ILogo } from "./types/types.component";
 
-const Logo: React.FC<ILogo> = ({ themeTitle, responsive }) => {
-	return <LogoView {... { themeTitle, responsive }} />;
+const Logo: React.FC<ILogo> = ({ themeTitle, responsive, isoType = false }) => {
+	return <LogoView {... { themeTitle, responsive, isoType }} />;
 };
 
 export default Logo;
--- src/components/Logo/styled.ts
+++ src/components/Logo/styled.ts
@@ -4,6 +4,10 @@ import { ILogoStyled } from "./types/types.styled";
 export const Logo = styled.div<ILogoStyled>`
     width: ${({responsive}) => responsive && "1.875rem"};
 
+	g[data-isotype="isotype-logo"] {
+		display: none;
+	}
+
     @media (max-width: 650px) {
         svg {
             g[data-name="text-logo"] {
