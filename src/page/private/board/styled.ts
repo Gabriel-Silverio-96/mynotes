bfb341bd6ecd27c0d4f6d168921916ea9c6818eb
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-07-09 23:07:49 -0300

    feat(board): main custom scrollbar

--- src/page/private/board/styled.ts
+++ src/page/private/board/styled.ts
@@ -11,4 +11,23 @@ export const Main = styled.main`
     height: 87vh;
 	padding: 1.6rem 1rem 0;
 	width: 100%;
+
+	::-webkit-scrollbar-thumb {
+		background-color: rgba(104, 104, 104, 30%);
+		border-radius: 2rem;
+		cursor: all-scroll;
+	}
+
+	::-webkit-scrollbar {
+		width: 0.5rem;
+		height: 0.2rem;
+		background-color: transparent;
+		border-radius: 2rem;
+		cursor: all-scroll;
+	}
+
+	::-webkit-scrollbar-track {
+		-webkit-box-shadow: inset 0 0 6px transparent;
+	}
 `;
+
